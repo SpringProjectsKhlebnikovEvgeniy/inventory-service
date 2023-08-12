5c3941071ce16e8dac31ff7b610dbb1dca0f648f
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-08-12 13:17:23 +0300

    Inventory Service Initial Commit.

---
+++ src/test/java/sprojects/productservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sprojects.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
